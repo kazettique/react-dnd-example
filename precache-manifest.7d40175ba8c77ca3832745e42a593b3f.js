90ac62535b959223d83c1d2bd1a5a0d91bf9ccee
Woody <woody.chang@cloud-interactive.com>
2020-04-23 13:07:56 +0800

    Updates

---
+++ precache-manifest.7d40175ba8c77ca3832745e42a593b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa743a5c6e1198126ef30eb4e04c8a37",
+    "url": "/react-dnd-example/index.html"
+  },
+  {
+    "revision": "1958b88e07e8cadeaa88",
+    "url": "/react-dnd-example/static/css/main.9abae355.chunk.css"
+  },
+  {
+    "revision": "11943a4a5e7d2b499308",
+    "url": "/react-dnd-example/static/js/2.82fb0ad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dnd-example/static/js/2.82fb0ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1958b88e07e8cadeaa88",
+    "url": "/react-dnd-example/static/js/main.fad6b118.chunk.js"
+  },
+  {
+    "revision": "5f7e5722e1de3dbc52cb",
+    "url": "/react-dnd-example/static/js/runtime-main.f41ba12c.js"
+  }
+]);
