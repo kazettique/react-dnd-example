c3c6cb4c8ebb512d55bdd66901e674c5c476bfaf
Woody <woody.chang@cloud-interactive.com>
2020-04-29 22:50:23 +0800

    add react-spring example component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Board from './components/Board'
+// import SpringDraggable from './components/SpringDraggable'
 
 function App() {
   return (
     <div className="App">
       <Board />
+      {/* <SpringDraggable /> */}
     </div>
   );
 }
