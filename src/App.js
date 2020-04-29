0df8dfda837c7f67daa89753b4012b64a263a05d
Woody <woody.chang@cloud-interactive.com>
2020-04-29 23:19:04 +0800

    add react-beautiful-dnd example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Board from './components/Board'
 // import SpringDraggable from './components/SpringDraggable'
+import BeautifulDraggable from './components/BeautifulDraggable'
 
 function App() {
   return (
     <div className="App">
       <Board />
       {/* <SpringDraggable /> */}
+      <BeautifulDraggable />
     </div>
   );
 }
