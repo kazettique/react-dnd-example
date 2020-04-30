15b4c7428be72c4a4779aeadf38a7c33986386ec
Woody <woody.chang@cloud-interactive.com>
2020-04-30 18:01:29 +0800

    delete some files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Board from './components/Board'
+// import Board from './components/Board'
 // import SpringDraggable from './components/SpringDraggable'
 // import BeautifulDraggable from './components/BeautifulDraggable'
 import BeautifulDraggable2 from './components/BeautifulDraggable2'
